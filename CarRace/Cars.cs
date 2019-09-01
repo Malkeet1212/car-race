58d44839cdff3383f7ec4357b2133fb954a34131
07ekb <07ekb@LAPTOP-HMFBP9KQ>
2019-09-02 11:47:26 +1200

    Add project files.

---
+++ CarRace/Cars.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace carrace
+{
+    class RACERS
+    {
+        public int AMOUNTRACER;
+        public RadioButton RADIOBTNS;
+        public Label LABLS;
+        public string RACERNAME;
+        public BETTING MONEYONCAR;
+        
+
+      
+
+
+        public void CANCELBET()
+        {
+            MONEYONCAR.MONEY = 0;
+        }
+        public RACERS(string BETTERNAME, int MONEY, RadioButton BTNRADIOS, Label LABELS)
+        {
+            RACERNAME = BETTERNAME;
+            AMOUNTRACER = MONEY;
+            LABLS = LABELS;
+            MONEYONCAR = new BETTING(this);
+            RADIOBTNS = BTNRADIOS;
+            
+        }
+        public bool BETAMOUNT(int mONEY, int FIRST)
+        {
+            MONEYONCAR.MONEY = mONEY;
+            MONEYONCAR.CARS = FIRST;
+            if (AMOUNTRACER >= mONEY) return true;
+            return false;
+        }
+        public void CARWINNER(int WINNED)
+        {
+            AMOUNTRACER += MONEYONCAR.RACEFINISH(WINNED);
+            CANCELBET();
+            LABELSUPDATESCAR();
+        }
+        public void LABELSUPDATESCAR()
+        {
+            LABLS.Text = MONEYONCAR.PAcedbet();
+            RADIOBTNS.Text = RACERNAME + " has " + AMOUNTRACER + " $";
+        }
+       
+    }
+}
---
+++ CarRace/person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace carrace
+{
+    public class person
+    {
+        public PictureBox CARPITURE;
+        public int CARSTART;
+        public int CARRACE;
+        public int POSTION;
+        public Random RANDOM;
+
+
+        public void go()
+        {
+            this.CARPITURE.Left = 0;
+            this.POSTION = 0;
+        }
+        public bool Raceing()
+        {
+            POSTION = RANDOM.Next(5);
+            CARPITURE.Left += CARSTART + POSTION;
+
+            if (CARPITURE.Left >= CARRACE)
+                return true;
+
+            return false;
+        }
+        
+
+       
+    }
+}
